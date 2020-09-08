4a59982a6f16e1af0bda6cd3be08212b17ab1f4c
Buck Tower <buck@bucktower.net>
2020-09-07 18:19:12 -0700

    Updates

---
+++ precache-manifest.5b4f0a59ab0c87839f6e4abac1357106.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd334085abf214f226d6dd278ba17da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f4b46b5bd910883f162",
+    "url": "/static/css/2.f1b50ea8.chunk.css"
+  },
+  {
+    "revision": "56a7a3d64782c8bddbfd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1f4b46b5bd910883f162",
+    "url": "/static/js/2.ba8ffa2e.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.ba8ffa2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a7a3d64782c8bddbfd",
+    "url": "/static/js/main.bb95369c.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
