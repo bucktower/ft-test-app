203f5f884d393361649457bd559c5ed4cd551faa
Peter Dun <pdun2008@gmail.com>
2021-01-09 22:20:05 -0500

    update test app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import 'reactjs-popup/dist/index.css';
 import { Feathery } from 'feathery-react-client-sdk';
 
+const sdkKey = '374bc28b-a18b-4b96-807e-3f3a8ab54faa'
+const userKey = 'test9'
+
 function App() {
-  // air.inc
-  return <Feathery
-    sdkKey='f22d8830-d101-4599-9023-97ea74aaa4d5'
-    userKey='test_user'
-    clientKey=''
-  />
+  Feathery.init(sdkKey, userKey);
+
+  const [attributes, setAttributes] = useState({});
+
+  useEffect(() => {
+    Feathery.fetchAttributes().then(newAttr => {setAttributes(newAttr)});
+  }, [])
+
+  return <Feathery.Div clientKey=''/>
 }
 
 export default App;
