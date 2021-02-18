3170a83121ecab84bc218c1eed808bd728888968
Peter Dun <pdun2008@gmail.com>
2021-02-18 18:33:20 -0500

    run local tests without bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import 'reactjs-popup/dist/index.css';
-import { Feathery } from 'feathery-react-client-sdk';
+import React from 'react';
+import { Feathery } from 'feathery-react';
 
-const sdkKey = '374bc28b-a18b-4b96-807e-3f3a8ab54faa'
-const userKey = 'test9'
+const sdkKey = '07a5797d-f8f8-4858-9ad6-2a2dd766d63e'
+const userKey = 'e'
 
 function App() {
   Feathery.init(sdkKey, userKey);
 
-  const [attributes, setAttributes] = useState({});
-
-  useEffect(() => {
-    Feathery.fetchAttributes().then(newAttr => {setAttributes(newAttr)});
-  }, [])
-
-  return <Feathery.Div clientKey=''/>
+  return <Feathery.Form formKey='screening_template'/>
 }
 
 export default App;
